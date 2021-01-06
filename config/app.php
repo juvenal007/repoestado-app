4207579891fbbccaaeb2d734448c3df60e4c58fe
juvenalSalas <juvenal519@gmail.com>
2021-01-06 06:10:13 -0300

    url2

--- config/app.php
+++ config/app.php
@@ -52,9 +52,9 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'https://repoestado-app.herokuapp.com/'),
+    'url' => env('APP_URL', 'http://localhost'),
 
-    'asset_url' => env('ASSET_URL', null),
+    'asset_url' => env('ASSET_URL', 'https://repoestado-app.herokuapp.com/'),
 
     /*
     |--------------------------------------------------------------------------
